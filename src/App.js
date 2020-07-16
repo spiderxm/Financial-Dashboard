002535d9d9e8f7c353da00dc61f32ea45961b042
Mrigank  Anand <mrigank.rkee@rediffmail.com>
2020-07-16 18:30:09 +0530

    adding loader

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import sma from 'sma'
 
 
 class App extends Component {
+
     state = {
         ticker: null,
         hidden1: true,
         hidden2: true,
         hidden3: true,
+        visible: true
 
     }
     ontickerchangeHandler = (event) => {
@@ -20,6 +22,7 @@ class App extends Component {
 
     financialDashboard = (event) => {
         event.preventDefault();
+        this.setState({visible: false});
         this.setState({hidden1: true, hidden2: true, hidden3: true})
         let low = [], high = [], close = [], open = [], volume = [], date;
         axios.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + this.state.ticker + "&outputsize=full&apikey=9O8KE4AAY96A5AKK")
@@ -35,7 +38,7 @@ class App extends Component {
                 }
                 console.log(std(high))
                 var layout = {
-                    width: '100%',
+                    width: 1440,
                 };
                 var DATA = [
                     {
@@ -46,6 +49,14 @@ class App extends Component {
                 ];
 
                 Plotly.newPlot('daily-open', DATA, layout);
+                var DATA = [
+                    {
+                        x: date,
+                        y: sma(open, 7),
+                        type: 'scatter',
+                    }
+                ];
+                Plotly.newPlot('daily-openMa', DATA, layout);
                 let ma1 = sma(volume, 7)
                 console.log(ma1)
                 var DATA = [
@@ -56,7 +67,7 @@ class App extends Component {
                     }
                 ];
 
-                Plotly.newPlot('daily-volumeMa', DATA);
+                Plotly.newPlot('daily-volumeMa', DATA, layout);
 
                 var DATA = [
                     {
@@ -66,7 +77,7 @@ class App extends Component {
                     }
                 ];
 
-                Plotly.newPlot('daily-volume', DATA);
+                Plotly.newPlot('daily-volume', DATA, layout);
                 var trace = {
                     x: date.reverse(),
                     close: close.reverse(),
@@ -82,6 +93,7 @@ class App extends Component {
                 };
                 var layout = {
                     dragmode: 'zoom',
+                    width: 1440,
                     margin: {
                         r: 10,
                         t: 25,
@@ -119,7 +131,9 @@ class App extends Component {
                     close[i] = data[date[i]]['4. close'];
                     volume[i] = data[date[i]]['5. volume'];
                 }
-
+                var layout = {
+                    width: 1440,
+                };
                 var DATA = [
                     {
                         x: date,
@@ -127,7 +141,7 @@ class App extends Component {
                         type: 'scatter',
                     }
                 ];
-                Plotly.newPlot('weekly-open', DATA);
+                Plotly.newPlot('weekly-open', DATA, layout);
                 var DATA = [
                     {
                         x: date,
@@ -135,7 +149,7 @@ class App extends Component {
                         type: 'scatter',
                     }
                 ];
-                Plotly.newPlot('weekly-close', DATA);
+                Plotly.newPlot('weekly-close', DATA, layout);
                 var DATA = [
                     {
                         x: date,
@@ -143,7 +157,7 @@ class App extends Component {
                         type: 'scatter',
                     }
                 ];
-                Plotly.newPlot('weekly-low', DATA);
+                Plotly.newPlot('weekly-low', DATA, layout);
                 var DATA = [
                     {
                         x: date,
@@ -151,7 +165,7 @@ class App extends Component {
                         type: 'scatter',
                     }
                 ];
-                Plotly.newPlot('weekly-high', DATA);
+                Plotly.newPlot('weekly-high', DATA, layout);
                 var DATA = [
                     {
                         x: date,
@@ -159,7 +173,7 @@ class App extends Component {
                         type: 'scatter',
                     }
                 ];
-                Plotly.newPlot('weekly-volume', DATA);
+                Plotly.newPlot('weekly-volume', DATA, layout);
                 this.setState({hidden2: false})
 
             })
@@ -183,9 +197,9 @@ class App extends Component {
                 var trace = {
                     x: date.reverse(),
                     close: close.reverse(),
-                    decreasing: {line: {color: '#ffcccb'}},
+                    decreasing: {line: {color: '#DC143C'}},
                     high: high.reverse(),
-                    increasing: {line: {color: '#90EE90'}},
+                    increasing: {line: {color: '#2E8B57'}},
                     line: {color: 'rgba(31,119,180,1)'},
                     low: low.reverse(),
                     open: open.reverse(),
@@ -225,40 +239,112 @@ class App extends Component {
         console.log(this.state.ticker);
     }
 
+
     render() {
-        let cont = this.state.content;
+        const style = {
+            border: "1px solid grey",
+            borderRadius: "20px",
+            padding: "1rem",
+            textAlign: "center",
+            margin: "auto",
+            boxShadow: "0px 2px 11px 6px rgba(0,0,0,.3)",
+            backgroundColor: "rgba(0,0,0,.85)",
+            color: "white",
+            marginBottom: "5rem",
+            width: '450px'
+        }
         let hidden = true;
         if (!this.state.hidden1 && !this.state.hidden2 && !this.state.hidden3) {
             hidden = false;
         }
+        let visible = true;
+        if (this.state.visible) {
+            visible = true;
+        } else {
+            visible = !hidden
+        }
+        console.log(visible)
         return (
             <React.Fragment>
-                <div>Finance Dashboard</div>
-                <form onSubmit={this.financialDashboard}>
-                    <input onChange={this.ontickerchangeHandler} value={this.state.ticker} required={true}/>
-                    <button type={"submit"}> Get financial data</button>
-                </form>
-                <h1 hidden={hidden}>Daily plots</h1>
-                <div id='myDiv1' hidden={hidden}></div>
-                <h2>Candle</h2>
-                <div id='myDiv'></div>
-                <h2>Daily open</h2>
-                <div id='daily-open'></div>
-                <h2>Daily Volume MA</h2>
-                <div id='daily-volumeMa'></div>
-                <h2>Daily Volume</h2>
-                <div id='daily-volume'></div>
-                <h2>Weekly high</h2>
-                <div id='weekly-high'></div>
-                <h2>Weekly low</h2>
-                <div id='weekly-low'></div>
-                <h2>Weekly close</h2>
-                <div id='weekly-close'></div>
-                <h2>Weekly open</h2>
-                <div id='weekly-open'></div>
-                <h2>Weekly volume traded</h2>
-                <div id='weekly-volume'></div>
-
+                <header>
+                    <img
+                        src="https://storage.cloud.google.com/applevggh/Financial-chart-economic-graph-analysis-512.png"
+                        alt={"icon"}/>
+                    <nav>
+                        <li><a>Financial Dashboard</a></li>
+                    </nav>
+                </header>
+                <div className="ui container left aligned" style={style}>
+                    <form onSubmit={this.financialDashboard}>
+                        <div className="field">
+                            <h1>Enter Ticker</h1>
+                            <div className="ui large icon input">
+                                <input onChange={this.ontickerchangeHandler} placeholder={'AMZN'}
+                                       value={this.state.ticker}
+                                       required={true}/>
+                                <i className="inverted circular search link icon" onClick={this.financialDashboard}></i>
+                            </div>
+                        </div>
+                        <button hidden="hidden" style={{display: "none"}} className="ui button basic inverted"
+                                type={"submit"}> Get financial data
+                        </button>
+                    </form>
+                </div>
+                <div className="ui segment" hidden={visible} style={{height: '400px'}}>
+                    <div className="ui active inverted dimmer">
+                        <div className="ui large text loader">Loading</div>
+                    </div>
+                </div>
+                <div hidden={hidden}>
+                    <h1>Daily plots</h1>
+                    <div id='myDiv1' hidden={hidden}></div>
+                    <div className="ui section divider"></div>
+                    <div style={{textAlign: "center"}}>
+                        <h2>Candle</h2>
+                        <div id='myDiv'></div>
+                        <div className="ui section divider"></div>
+                    </div>
+                    <div style={{textAlign: "center"}}>
+                        <h2>Daily open</h2>
+                        <div id='daily-open'></div>
+                        <div className="ui section divider"></div>
+                    </div>
+                    <h2>Daily open MA</h2>
+                    <div id='daily-openMa'></div>
+                    <div className="ui section divider"></div>
+                    <h2>Daily Volume MA</h2>
+                    <div id='daily-volumeMa'></div>
+                    <div className="ui section divider"></div>
+                    <div style={{textAlign: "center"}}>
+                        <h2>Daily Volume</h2>
+                        <div id='daily-volume'></div>
+                        <div className="ui section divider"></div>
+                    </div>
+                    <div style={{textAlign: "center"}}>
+                        <h2>Weekly high</h2>
+                        <div id='weekly-high'></div>
+                        <div className="ui section divider"></div>
+                    </div>
+                    <div style={{textAlign: "center"}}>
+                        <h2>Weekly low</h2>
+                        <div id='weekly-low'></div>
+                        <div className="ui section divider"></div>
+                    </div>
+                    <div style={{textAlign: "center"}}>
+                        <h2>Weekly close</h2>
+                        <div id='weekly-close'></div>
+                        <div className="ui section divider"></div>
+                    </div>
+                    <div style={{textAlign: "center"}}>
+                        <h2>Weekly open</h2>
+                        <div id='weekly-open'></div>
+                        <div className="ui section divider"></div>
+                    </div>
+                    <div style={{textAlign: "center"}}>
+                        <h2>Weekly volume traded</h2>
+                        <div id='weekly-volume'></div>
+                    </div>
+                </div>
 
             </React.Fragment>
         )
